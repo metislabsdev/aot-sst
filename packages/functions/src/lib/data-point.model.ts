b0a52ade03bcacf4c8a37da54b5dddec4ffa7e97
msantore <2057610+msantore@users.noreply.github.com>
2025-01-02 13:49:14 -0800

    chore: misc

--- packages/functions/src/lib/data-point.model.ts
+++ packages/functions/src/lib/data-point.model.ts
@@ -2,12 +2,15 @@ import { DATA_SOURCES } from "./connectors/dynamo/models/data-sources";
 import { INVESTMENT_DATA_TYPES } from "./connectors/dynamo/models/investments.model";
 
 export interface DataPoint {
+  name: string; // enum name for the data point
   value: number;
   timestamp: number;
   frequency: Frequency;
   InvestmentType: INVESTMENT_DATA_TYPES;
   source: DATA_SOURCES;
   type: INVESTMENT_DATA_TYPES;
+  dataSource: DATA_SOURCES;
+  payload: string; // json blob of api response if needed
 }
 
 enum Frequency {
@@ -17,5 +20,3 @@ enum Frequency {
   MONTHLY = "MONTHLY",
   YEARLY = "YEARLY",
 }
-
-export enum DATA_POINT_TYPES {}
