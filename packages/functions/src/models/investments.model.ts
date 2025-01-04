561c5bab09fda07eb08ad531f499c9d4c7a78691
MSantore <2057610+msantore@users.noreply.github.com>
2025-01-04 11:50:45 -0800

    Data Model (#1)

---
+++ packages/functions/src/models/investments.model.ts
@@ -0,0 +1,10 @@
+export enum INVESTMENT_DATA_TYPES {
+  ASSET = "ASSET", // General overview for an asset(Property, stock, crypto, etc) (TESLA, BTC, etc  )
+  MACRO = "MACRO", // General overview for a macro financial data (GDP, Unemployment, etc)
+}
+
+export enum ASSET_CLASSES {
+  CRYPTO = "CRYPTO",
+  STOCK = "STOCK",
+  REAL_ESTATE = "REAL_ESTATE",
+}
